47d3ae29e958675c53351ba6ef84c4f0057e0ff9
rank <rank@ubuntu.myguest.virtualbox.org>
2023-04-04 02:04:05 +0530

    updated with l8

--- todo-app/app.js
+++ todo-app/app.js
@@ -9,7 +9,7 @@ app.use(bodyParser.json());
 
 app.set("view engine","ejs");
 
-app.use(express.static(path.join(__dirname,"public")))
+app.use(express.static(path.join(__dirname + "/public")))
 
 
 
